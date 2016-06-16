bfed8774b6afea5923ccc6c5caca74c4a9f84e19
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-15 22:31:34 -0400

    adding sql file

---
+++ txt.sql
@@ -0,0 +1,9 @@
+USE Bamazon;
+
+CREATE TABLE products (
+ItemID VARCHAR(30) NOT NULL,
+ProductName VARCHAR(30) NOT NULL,
+DepartmentName VARCHAR(30) NOT NULL,
+Price INTEGER(1000),
+StockQuantity INTEGER(30)
+);
