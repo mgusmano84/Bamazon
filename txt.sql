0aac1d012ed333c93a4a2afa1c4072eecb2966a0
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-16 10:57:04 -0400

    adding inventory

--- txt.sql
+++ txt.sql
@@ -1,9 +1,16 @@
 USE Bamazon;
 
-CREATE TABLE products (
-ItemID VARCHAR(30) NOT NULL,
-ProductName VARCHAR(30) NOT NULL,
-DepartmentName VARCHAR(30) NOT NULL,
-Price INTEGER(1000),
-StockQuantity INTEGER(30)
-);
+-- CREATE TABLE products (
+-- ItemID VARCHAR(30) NOT NULL,
+-- ProductName VARCHAR(30) NOT NULL,
+-- DepartmentName VARCHAR(30) NOT NULL,
+-- Price INTEGER(1000),
+-- StockQuantity INTEGER(100)
+-- );
+-- 
+INSERT INTO products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("3002", "Blue Tie", "Clothing", 12, 150);
+
+SELECT * FROM products;
+
+DELETE FROM products WHERE StockQuantity = 200;
