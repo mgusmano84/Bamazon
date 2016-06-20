c56ee45601e70c0b77f141a155306d140f9799bc
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-20 16:09:17 -0400

    able to make selections of items

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -19,12 +19,67 @@ connection.connect(function(err) {
 var InvSearch = function() {
         var query = 'SELECT * FROM products';
         connection.query(query, function(err, res) {
+        	console.log(' Available Products')
+            console.log('***********************************')
             for (var i = 0; i < res.length; i++) {
-                console.log(res[i]);
+                console.log("Item ID:  " + res[i].ItemID + " || Product Name: " + res[i].ProductName + " || Department: " + res[i].DepartmentName + " || Price: " + res[i].Price + " || Quanity: " + res[i].StockQuantity);
             }
         })
+        selectItem();
 };
 
 
+var selectItem = function() {
+		
+		inquirer.prompt([{
+			name: 'id',
+			type: 'input',
+			message: 'Please Select ID of the item you would like to by?',
+			validate: function(value) {
 
-// Item ID: " + res[i].ItemID + " || Product Name: " + res[i].ProductName + " || Department: " + res[i].DepartmentName + " || Price: " + res[i].Price + " || Quanity: " + res[i].StockQuantity
+				
+				if (isNaN(value) == false) {
+
+					
+					return true;
+
+				} else {
+					console.log('\n\nAll we need is the number next to the title.\n');
+					return false;
+
+				} // end if else
+
+			} // end validate()
+		}, {
+			name: 'amount',
+			type: 'input',
+			message: 'How many would you like to buy?',
+			validate: function(value) {
+
+				
+				if (isNaN(value) == false) {
+
+					
+					return true;
+
+				} else {
+
+					
+					console.log('\nPlease supply ID\n');
+					return false;
+
+				} 
+
+			} 
+		
+		}]).then(function(answer) {
+
+			
+			console.log(answer);
+		
+		});  
+
+}; 
+
+
+
