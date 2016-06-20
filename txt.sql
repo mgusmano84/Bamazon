75d0402ffb5ce1788258f21d9f6975864f0032f7
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-20 08:29:48 -0400

    updated inventory

--- txt.sql
+++ txt.sql
@@ -1,16 +1,19 @@
 USE Bamazon;
 
--- CREATE TABLE products (
--- ItemID VARCHAR(30) NOT NULL,
--- ProductName VARCHAR(30) NOT NULL,
--- DepartmentName VARCHAR(30) NOT NULL,
--- Price INTEGER(1000),
--- StockQuantity INTEGER(100)
--- );
--- 
+CREATE TABLE products (
+ItemID INTEGER(10) AUTO_INCREMENT NOT NULL,
+ProductName VARCHAR(30) NOT NULL,
+DepartmentName VARCHAR(30) NOT NULL,
+Price DECIMAL(10,2) NULL,
+StockQuantity INTEGER(10),
+PRIMARY KEY (ItemID)
+);
+
 INSERT INTO products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
 VALUES ("3002", "Blue Tie", "Clothing", 12, 150);
 
 SELECT * FROM products;
 
 DELETE FROM products WHERE StockQuantity = 200;
+
+DROP TABLE products;
