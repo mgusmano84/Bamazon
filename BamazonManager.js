50c9434569c7ee8b8a5be1bf6559525b8f8a360f
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-20 23:06:45 -0400

    added manager js

---
+++ BamazonManager.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", 
+    password: process.argv[2], 
+    database: "Bamazon"
+})
+
+
+var choices = function () {
+	inquirer.prompt([
+		{
+        type: "list",
+        message: "Welcome to Bamazon Manager, what would you like to do?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        name: "pokemon"
+    	}
+	]).then(function (choice) {
+
+	})
+
+}
